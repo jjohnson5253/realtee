229c5f1ea20ad7eb3baff180c831cb3fe2431199
Jake Johnson <jakejohnson@jakes-macbook-pro.local>
2018-06-17 09:01:41 -0500

    carousel in the works

--- RealtyAppPrototype/RealtyAppPrototype/Controllers/LoginViewController.swift
+++ RealtyAppPrototype/RealtyAppPrototype/Controllers/LoginViewController.swift
@@ -7,16 +7,74 @@
 //
 
 import UIKit
+import Foundation
 //import Firebase
 
 class LoginViewController: UIViewController {
     
     // MARK: Constants
     let loginToList = "LoginToList"
+    let backGroundImageArray : [UIImage] = [UIImage(named: "carousel1")!, UIImage(named: "carousel2")!, UIImage(named: "carousel3")!]
+    
+    // MARK: Variables
+    var backGroundImageArrayIndex : Int = 0
+    var timer: Timer!
     
     // MARK: Outlets
     @IBOutlet weak var textFieldLoginEmail: UITextField!
     @IBOutlet weak var textFieldLoginPassword: UITextField!
+    @IBOutlet weak var backGround: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        
+//        timer = Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(self.changeCarousel), userInfo: nil, repeats: true)
+
+//        backGround.animationImages = backGroundImageArray
+//        backGround.animationDuration = 3
+//        backGround.ani
+//        backGround.startAnimating()
+        
+        UIView.transition(with: self.backGround,
+                          duration:1,
+                          options: .transitionCrossDissolve,
+                          animations: { self.backGround.image = self.backGroundImageArray[self.backGroundImageArrayIndex] },
+                          completion: nil)
+        
+    }
+    
+    override func didMove(toParentViewController parent: UIViewController?) {
+//        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.changeCarousel), userInfo: nil, repeats: true)
+
+    }
+    
+    @objc func changeCarousel(){
+        
+        print("counted")
+        
+        if backGroundImageArrayIndex > 2 {backGroundImageArrayIndex = 0}
+//
+//        UIView.animate(withDuration: 0.3, animations: {
+//            self.backGround.image = self.backGroundImageArray[self.backGroundImageArrayIndex]
+//        })
+        
+        UIView.transition(with: self.backGround,
+                          duration:1,
+                          options: .transitionCrossDissolve,
+                          animations: { self.backGround.image = self.backGroundImageArray[self.backGroundImageArrayIndex] },
+                          completion: nil)
+        
+//        UIView.transition(with: imageView,
+//                          duration: 0.75,
+//                          options: .transitionCrossDissolve,
+//                          animations: { self.imageView.image = toImage },
+//                          completion: nil)
+
+        
+        backGroundImageArrayIndex = backGroundImageArrayIndex + 1
+    
+    }
+
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
